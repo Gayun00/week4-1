e140ba07238baa58ed36359999d768af445e33da
gy <gayunlee11@gmail.com>
2022-02-21 23:52:45 +0900

    Feat: 클래스 레이아웃

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const test = 123;
-let test2 = 456;
-console.log(test, test2);
+const mainImgContainer = document.querySelector('.sub-carousel__div--image-wrap');
+const subImgContainer = document.querySelectorAll('.sub-carousel__div--image-wrap')
+let classesArr = [];
+
+async function fetchClass() {
+  const res = await fetch('./data.json');
+  const classesData = await res.json();
+  classesArr = classesData;
+}
+fetchClass()
+
+function createClassesEl() {
+  const imgEl = document.createElement('img');
+  //
+
+}
